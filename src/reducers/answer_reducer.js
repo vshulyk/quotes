53ad01a69db5a062501f2fac9a54b3bb6cbee01b
Michael Plakhov <qotsaman@gmail.com>
2016-08-01 10:20:57 +0300

    Update answer_reducer.js
    Fixes sticky CHECK button label issue

--- src/reducers/answer_reducer.js
+++ src/reducers/answer_reducer.js
@@ -1,10 +1,13 @@
 import { ANSWER_CHANGE } from '../actions/answer_change_action';
+import { MARK_SOLVED } from '../actions/mark_solved_action';
 
 export default function( state = '', action ) {
 
   switch( action.type ) {
     case ANSWER_CHANGE:
       return action.payload;
+    case MARK_SOLVED:
+      return "";  
   };
 
   return state;
